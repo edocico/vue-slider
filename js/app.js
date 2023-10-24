5edd02052546d650e4a9b44feb75e4d9a203abf3
edocico <138378767+edocico@users.noreply.github.com>
2023-10-24 17:03:07 +0200

    add click function to thumbnail

--- js/app.js
+++ js/app.js
@@ -10,5 +10,22 @@ const { createApp } = Vue
             texts : ['Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.','Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.','Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.','Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',"Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay."],
         }
       }
+    },
+    methods: {
+        getNextPic() {
+            this.imageCounter++
+            if (this.imageCounter >= this.images.image.length) {
+                this.imageCounter = 0
+            }
+        },
+        getPrevPic() {
+            this.imageCounter--
+            if (this.imageCounter < 0) {
+                this.imageCounter = this.images.image.length - 1
+            }
+        },
+        clickToChange(thumbindex) {
+            this.imageCounter = thumbindex;
+        }
     }
   }).mount('#app')
